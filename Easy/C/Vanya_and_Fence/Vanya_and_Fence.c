9a89af2610edef604df150495a5834fa5511a38c
Dallal1 <abw090571@gmail.com>
2024-01-03 23:24:28 +0100

    A Add C

---
+++ Easy/C/Vanya_and_Fence/Vanya_and_Fence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int h;
+	int i;
+	int n;
+
+	printf("Enter The Number Frind: ");
+	scanf("%d", &i);
+	printf("Enter The Higet: ");
+	scanf("%d", &h);
+	printf("%d", i);
+	printf ("%d", h);
+
+	return (0);
+}
