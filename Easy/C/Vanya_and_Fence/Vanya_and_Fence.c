3791c2219bab7cda5027a683d4f51ecbf68a6cea
Dallal1 <abw090571@gmail.com>
2024-01-04 01:36:04 +0100

    C

--- Easy/C/Vanya_and_Fence/Vanya_and_Fence.c
+++ Easy/C/Vanya_and_Fence/Vanya_and_Fence.c
@@ -5,14 +5,39 @@ int main()
 {
 	int h;
 	int i;
-	int n;
 
-	printf("Enter The Number Frind: ");
+	printf("Enter The Number Frend: ");
 	scanf("%d", &i);
 	printf("Enter The Higet: ");
 	scanf("%d", &h);
-	printf("%d", i);
-	printf ("%d", h);
+	printf("%d %d\n", i, h);
+	
+	int n[i];
+	int l;
+	int top;
 
+	top = 0;
+	l = 0;
+	for(int j = 0; j < i; j++)
+	{
+		scanf("%d", &n[j]);
+	}
+	while (n[l] != '\0')
+	{
+		if (n[l] > h)
+		{
+			top = top + 2;	
+		}
+		if (n[l] <= h)
+		{
+			top = top + 1;
+		}
+		l++;
+	}
+	for(int g = 0; g < i; g++)
+	{
+		printf ("%d ", n[g]);
+	}
+	printf ("\n%d", top);
 	return (0);
 }
